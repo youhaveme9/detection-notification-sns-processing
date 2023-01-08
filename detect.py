5d1f96313ae34c958dbd22b3761040b2f572bec4
Roshan Kumar <roshaen09@gmail.com>
2023-01-09 00:02:50 +0530

    added continuous detection feature

--- detect.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-Hearder = {'X-Api-Key': '/mlYOZmBL3SZgvnSFiJOZA==1rSMVYLqMGGCFFX2'}
-api_url = 'https://api.api-ninjas.com/v1/imagetotext'
-image_file_descriptor = open('runs/detect/exp38/crops/plate/image0.jpg', 'rb')
-files = {'image': image_file_descriptor}
-r = requests.post(api_url, files=files, headers=Hearder)
-print(r.json())
